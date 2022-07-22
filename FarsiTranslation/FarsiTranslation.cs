5c21eaa8fc5e8aca15ea25ca10d919e397094fa4
Nick <nicholasconnors@hotmail.ca>
2022-07-22 12:44:50 -0400

    Implemented the mod

---
+++ FarsiTranslation/FarsiTranslation.cs
@@ -0,0 +1,16 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+
+namespace FarsiTranslation
+{
+    public class FarsiTranslation : ModBehaviour
+    {
+        private void Start()
+        {
+            ModHelper.Console.WriteLine($"{nameof(FarsiTranslation)} is loaded!", MessageType.Success);
+
+            var api = ModHelper.Interaction.TryGetModApi<ILocalizationAPI>("xen.LocalizationUtility");
+            api.RegisterLanguage(this, "Arabic", "assets/Translations.xml", "assets/b_mitra_0", "Assets/B_MITRA_0.TTF", ArabicSupport.ArabicFixer.Fix);
+        }
+    }
+}
---
+++ FarsiTranslation/ILocalizationAPI.cs
@@ -0,0 +1,12 @@
+﻿using OWML.ModHelper;
+using System;
+
+namespace FarsiTranslation
+{
+    public interface ILocalizationAPI
+    {
+        void RegisterLanguage(ModBehaviour mod, string name, string translationPath, string assetBundlePath, string fontPath, Func<string, string> fixer);
+        void RegisterLanguage(ModBehaviour mod, string name, string translationPath, string assetBundlePath, string fontPath);
+        void RegisterLanguage(ModBehaviour mod, string name, string translationPath);
+    }
+}
