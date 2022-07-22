ab2c08853109b64e9a7e6f18ee4421511ac49b91
Noah Pilarski <mehmehmeh313@gmail.com>
2022-07-22 14:27:51 -0400

    Simplify

--- FarsiTranslation/FarsiTranslation.cs
+++ FarsiTranslation/FarsiTranslation.cs
@@ -9,8 +9,7 @@ namespace FarsiTranslation
         {
             ModHelper.Console.WriteLine($"{nameof(FarsiTranslation)} is loaded!", MessageType.Success);
 
-            var api = ModHelper.Interaction.TryGetModApi<ILocalizationAPI>("xen.LocalizationUtility");
-            api.RegisterLanguage(this, "Arabic", "assets/Translations.xml", "assets/b_mitra_0", "Assets/B_MITRA_0.TTF", ArabicSupport.ArabicFixer.Fix);
+            ModHelper.Interaction.TryGetModApi<ILocalizationAPI>("xen.LocalizationUtility").RegisterLanguage(this, "Arabic", "assets/Translations.xml", "assets/b_mitra_0", "Assets/B_MITRA_0.TTF", ArabicSupport.ArabicFixer.Fix);
         }
     }
 }
